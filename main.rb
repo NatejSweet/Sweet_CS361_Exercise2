756e76dc1f29f0f72875fc4da8905c5618a26d6b
Nathan Sweet <sweetn@oregonstate.edu>
2023-10-09 11:11:02 -0700

    further reorganized, removed unnecessary local variables

--- main.rb
+++ main.rb
@@ -5,14 +5,13 @@
 def modified_name(choose) #modifies a name based on passed boolean
   split_name = @name.split
   if choose     #if user chooses alternate modification, simply append borg to the name and return it
-    reversed_name_components2 = split_name
-    reversed_name_components2 << "Borg"
-    new_name2 = reversed_name_components2.join(' ')
-    return new_name2
+    split_name << "Borg"
+    new_name = split_name.join(' ')
+    return new_name
   end    #if user does not choose alternate modification, return reversed name
-  reversed_name_components = split_name.reverse
-  new_name1 = reversed_name_components.join(' ')
-  return new_name1
+  reversed_name = split_name.reverse()
+  new_name = reversed_name.join(' ')
+  return new_name
 end
 
 puts "New name: #{modified_name(false)}"
